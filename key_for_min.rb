15c8d47320f22e6e72ee4f4a1de56d3f2ad3070f
Aiden <aiden.marques94@gmail.com>
2020-11-18 11:47:57 -0500

    commit tree 3364d9a6e912cc8eaf2dd66b6c8bec64eb6e23d9

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,15 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-
-end
+    def key_for_min_value(hash)
+        smallest = nil
+        key = nil
+        hash.collect do |name, val|
+          if smallest == nil || val < smallest
+            smallest = val
+            key = name
+          end
+        end
+        key
+      end
+    
